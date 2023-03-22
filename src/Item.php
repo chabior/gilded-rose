125de46acc090bf0bd10d98fd8898a75490c3829
Paweł Chabierski <pawel@appjobs.com>
2023-03-22 16:45:24 +0100

    make item data protected

--- src/Item.php
+++ src/Item.php
@@ -5,10 +5,35 @@ namespace App;
 final class Item
 {
     public function __construct(
-        public string $name,
-        public int $sell_in,
-        public int $quality
+        private readonly string $name,
+        private int $sell_in,
+        private int $quality
     ) {
+    }
+
+    public function getSellIn(): int
+    {
+        return $this->sell_in;
+    }
+
+    public function getQuality(): int
+    {
+        return $this->quality;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setQuality(int $quality): void
+    {
+        $this->quality = $quality;
+    }
+
+    public function setSellIn(int $sell_in): void
+    {
+        $this->sell_in = $sell_in;
     }
 
     public function __toString(): string
